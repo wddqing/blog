baffeb01cb8e8c5c08d2c31ff629f7350393e217
wddqing <wddqing@gmail.com>
2014-06-06 09:12:16 +0800

    modify passage

--- model/passage.js
+++ model/passage.js
@@ -9,23 +9,23 @@ module.exports = function passage(){
 	this.author = "";
 	this.content = "";
 	this.updateTime = "";
-	this.order = 0;
+	this.order = 0;//排序
 }
 
 passage.prototype.save = function(){
 
 }
 
-passage.prototype.del = function(pid){
+passage.prototype.del = function(){
 	
 }
 
-passage.prototype.modify = function(pid){
+passage.prototype.modify = function(){
 	
 }
 //根据文章id获取一篇文章
-passage.prototype.find = function(pid){
-	
+passage.prototype.find = function(id){
+	mysql.query()
 }
 //根据页数获取文章列表
 passage.prototype.list = function(page,pagesize){
